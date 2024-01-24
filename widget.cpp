0ff95d265660ebfa2a0f92fc84101fc33944379f
wupengfei <2736292553@qq.com>
2024-01-24 16:50:26 +0800

    初步完成

--- widget.cpp
+++ widget.cpp
@@ -1,15 +1,276 @@
 #include "widget.h"
 #include "ui_widget.h"
-
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    QStringList comboxData = {"car1", "car2", "car3", "car4", "car5", "car6"};
+    ui->comboBox->addItems(comboxData);
+    data = QByteArray::fromHex("f2 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 f6");
+    //qDebug() << "data.length:" << data.length();
+    serialPort.setPortName("/dev/ttyS0"); // 串口号，请根据实际情况修改
+    serialPort.setBaudRate(QSerialPort::Baud19200);
+    serialPort.setDataBits(QSerialPort::Data8);
+    serialPort.setParity(QSerialPort::EvenParity);
+    serialPort.setStopBits(QSerialPort::OneStop);
+    serialPort.setFlowControl(QSerialPort::NoFlowControl);
+    serialPort.open(QIODevice::ReadWrite);
+    connect(&serialPort, &QSerialPort::readyRead, this, &Widget::handleReadyRead);
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Widget::sendData);
+    timer->start(200);
 }
 
 Widget::~Widget()
 {
+    serialPort.close();
     delete ui;
 }
 
+quint16 Widget::calculateCRC(const QByteArray &data)
+{
+    quint16 checksum = 0xFFFF;
+    for (int i = 1; i < data.size() - 3; ++i)
+    {
+        checksum ^= quint8(data.at(i)) << 8;
+        for (int j = 0; j < 8; ++j)
+        {
+            checksum = (checksum & 0x8000) ? ((checksum << 1) ^ 0x1021) : (checksum << 1);
+        }
+    }
+    return checksum;
+}
+
+void Widget::on_recoveryData_clicked()
+{
+    data = emptyData;
+}
+
+void  Widget::handleReadyRead()
+{
+    QByteArray receiveData = serialPort.readAll();
+    // 处理收到的数据，这里简单地打印出来
+    qDebug() << "Received data: " << data.toHex();
+}
+
+void Widget::sendData()
+{
+    count++;
+    qDebug() << count << endl;
+    data[2] = quint8((count >> 8) & 0xFF);
+    data[3] = quint8(count & 0xFF);
+    quint16 checksum = calculateCRC(data);
+
+    // 更新校验位
+    data[data.size() - 3] = quint8((checksum >> 8) & 0xFF);
+    data[data.size() - 2] = quint8(checksum & 0xFF);
+    // 发送数据
+    serialPort.write(data);
+    qDebug() << "Sent data: " << data.toHex();
+}
+
+void Widget::on_door1_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | 1;
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | 1;
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | 1;
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | 1;
+    }
+    else {
+        ;
+    }
+    //data[5] = data[1] | 1;
+    //qDebug() << data.toHex() ;
+}
+
+void Widget::on_door2_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | (1 << 1);
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | (1 << 1);
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | (1 << 1);
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | (1 << 1);
+    }
+    else {
+        ;
+    }
+}
+
+
+void Widget::on_door3_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | (1 << 2);
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | (1 << 2);
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | (1 << 2);
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | (1 << 2);
+    }
+    else {
+        ;
+    }
+}
+
+void Widget::on_door4_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | (1 << 3);
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | (1 << 3);
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | (1 << 3);
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | (1 << 3);
+    }
+    else {
+        ;
+    }
+}
+
+void Widget::on_door5_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | (1 << 4);
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | (1 << 4);
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | (1 << 4);
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | (1 << 4);
+    }
+    else {
+        ;
+    }
+}
+
+void Widget::on_door6_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | (1 << 5);
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | (1 << 5);
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | (1 << 5);
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | (1 << 5);
+    }
+    else {
+        ;
+    }
+}
+
+void Widget::on_door7_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | (1 << 6);
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | (1 << 6);
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | (1 << 6);
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | (1 << 6);
+    }
+    else {
+        ;
+    }
+}
+
+void Widget::on_door8_clicked()
+{
+    data[4] = data[4] | (1 << 2);
+    if(ui->comboBox->currentIndex() == 0)
+    {
+        data[5] = data[5] | (1 << 7);
+    }
+    else if (ui->comboBox->currentIndex() == 1)
+    {
+        data[6] = data[6] | (1 << 7);
+    }
+    else if (ui->comboBox->currentIndex() == 2)
+    {
+        data[7] = data[7] | (1 << 7);
+    }
+    else if (ui->comboBox->currentIndex() == 3)
+    {
+        data[8] = data[8] | (1 << 7);
+    }
+    else {
+        ;
+    }
+}
+
+void Widget::on_car1_front_fault_clicked()
+{
+    data[4] = data[4] | (1 << 0);
+}
+
+void Widget::on_car2_door2_fault_clicked()
+{
+    data[4] = data[4] | (1 << 1);
+}
--- widget.h
+++ widget.h
@@ -2,7 +2,11 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include <QByteArray>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QtDebug>
+#include <QTimer>
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
@@ -13,9 +17,42 @@ class Widget : public QWidget
 
 public:
     Widget(QWidget *parent = nullptr);
+    quint16 calculateCRC(const QByteArray &data);
+
     ~Widget();
+
+private slots:
+    void handleReadyRead();
+
+    void sendData();
+
+    void on_door1_clicked();
+
+    void on_door2_clicked();
+
+    void on_recoveryData_clicked();
+
+    void on_door3_clicked();
+
+    void on_door4_clicked();
+
+    void on_door5_clicked();
+
+    void on_door6_clicked();
+
+    void on_door7_clicked();
+
+    void on_door8_clicked();
+
+    void on_car1_front_fault_clicked();
+
+    void on_car2_door2_fault_clicked();
 
 private:
     Ui::Widget *ui;
+    QByteArray data;
+    QSerialPort serialPort;
+    quint16 count = 0;
+    const QByteArray emptyData = QByteArray::fromHex("f2 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 f6");
 };
 #endif // WIDGET_H
